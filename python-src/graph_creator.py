324a7924101a34f8a6ab0d19ba95cfa1b2cac465
Lazova Verica <vericalcroft@gmail.com>
2015-06-05 17:53:57 +0200

    Building graph with Jaccard hybrid metric

--- python-src/graph_creator.py
+++ python-src/graph_creator.py
@@ -10,15 +10,28 @@ from interaction_graph_builder import *
 
 #-------------------------------------------------------------------------------
 
+def save_matrix_to_edgelist(M, path):
+    """ Save the matrix M as edgelist at the given path """
+    tmp = M.tocoo()
+    with open(path, 'w') as out:
+        for i, j, w in itertools.izip(tmp.row, tmp.col, tmp.data):
+            out.write('%d %d %f\n' % (i, j, w))
+
+
+#-------------------------------------------------------------------------------
+
 def jaccard(set1, set2):
+    """ Calculate the Jaccard similarity metric of two sets """
     intersect = float(len(set1.intersection(set2)))
     J = (intersect / len(set1) + intersect / len(set2)) * 0.5
     return J
 
 
+#-------------------------------------------------------------------------------
+
 def graph_content_jaccard(graph, id_to_protein, annotation_file, path):
     """ Builds interaction graph with content based weighs using Jaccard
-        similarity metric. """
+        similarity metric, and save it at the given path """
     protein_to_functions = read_in_annotations(annotation_file)
     with open(path, 'w') as out:
         for e in graph.edges_iter():
@@ -35,7 +48,7 @@ def graph_content_jaccard(graph, id_to_protein, annotation_file, path):
 
 def graph_structure_jaccard(graph, graph_jaccard, path):
     """ Builds interaction graph with structure based weighs using Jaccard
-        similarity metric. """
+        similarity metric, and save it at the given path """
     A = scipy.sparse.csr_matrix(nx.google_matrix(graph, alpha = 1, weight = 'weight'))
     W = nx.to_scipy_sparse_matrix(graph_jaccard)
     W2 = ( (W * A) + (A.transpose() * W) )*0.5
@@ -45,10 +58,19 @@ def graph_structure_jaccard(graph, graph_jaccard, path):
     max_w2 = W2.max()
     W2 = W2.multiply(max_w / max_w2)
 
-    tmp = W2.tocoo()
-    with open(path, 'w') as out:
-        for i, j, w in itertools.izip(tmp.row, tmp.col, tmp.data):
-            out.write('%d %d %f\n' % (i, j, w))
+    save_matrix_to_edgelist(W2, path)
+
+
+#-------------------------------------------------------------------------------
+
+def graph_hybrid_jaccard(content_graph, structure_graph, path):
+    """ Builds interaction graph with hybrid based weighs using Jaccard
+        similarity metric, and save it at the given path """
+    W1 = nx.to_scipy_sparse_matrix(content_graph)
+    W2 = nx.to_scipy_sparse_matrix(structure_graph)
+    W3 = (W1 + W2) * 0.5
+
+    save_matrix_to_edgelist(W3, path)
 
 
 #-------------------------------------------------------------------------------
@@ -57,16 +79,17 @@ def main():
     G, id_to_protein = build_graph('../data/human_ppi_data_900')
     #go900 = '../data/go/split/human_ppi900_go_mf_clean.tsv'
     #graph_content_jaccard(G, id_to_protein, go900, 'graphs/jaccard_content_900')
-
     G1 = build_graph_from_edgelist('graphs/jaccard_content_900', G.order())
-    graph_structure_jaccard(G, G1, 'graphs/jaccard_structure_900')
+    #graph_structure_jaccard(G, G1, 'graphs/jaccard_structure_900')
     G2 = build_graph_from_edgelist('graphs/jaccard_structure_900', G.order())
+    graph_hybrid_jaccard(G1, G2, 'graphs/jaccard_hybrid_900')
+    G3 = build_graph_from_edgelist('graphs/jaccard_hybrid_900', G.order())
 
-    print nx.info(G)
-    print
     print nx.info(G1)
     print
     print nx.info(G2)
+    print
+    print nx.info(G3)
 
 
 #-------------------------------------------------------------------------------
